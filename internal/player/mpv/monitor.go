mpldr/uniview

commit 84cbb77e5d895aff9b247fbf18f443ec1977c9d5

client: drop remotely triggered seeks and pauses
The current solution via a timeout is error prone and overall inelegant.
To remedy this, replace the timeout with an internal check to drop the
event.

internal/player/mpv/monitor.go
@@ -32,6 +32,11 @@ func (p *MPV) monitor() {
 
 		switch {
 		case res.Event == "seek":
+			if !p.dropSeek.Load() {
+				glog.Trace("dropped seek")
+				p.dropSeek.Store(false)
+				continue
+			}
 			select {
 			case p.notifySeekInternal <- struct{}{}:
 			default:
@@ -74,6 +79,10 @@ func (p *MPV) pollPause() {
 		if pause, ok := res.Data.(bool); ok {
 			if pause != pauseState {
 				pauseState = pause
+				if p.dropPause.Load() {
+					p.dropPause.Store(false)
+					continue
+				}
 				select {
 				case p.notifyPause <- pauseState:
 				default:

client.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"sync/atomic"
-	"time"
 
 	"git.sr.ht/~mpldr/uniview/internal/player"
 	"git.sr.ht/~mpldr/uniview/internal/player/mpv"
@@ -72,19 +70,13 @@ func startClient() error {
 		return fmt.Errorf("failed to join room: %w", err)
 	}
 
-	ignoreSeek := &atomic.Bool{}
-
 	glog.Debug("waiting for remote events…")
-	go recvChanges(stream, p, ignoreSeek)
+	go recvChanges(stream, p)
 
 	glog.Debug("waiting for player events…")
 	for {
 		select {
 		case timestamp := <-p.NotifySeek():
-			if ignoreSeek.Load() {
-				glog.Debug("seek ignored")
-				continue
-			}
 			glog.Debugf("local: seek to %s detected", timestamp)
 			stream.Send(&protocol.RoomEvent{
 				Type: protocol.EventType_EVENT_JUMP,
@@ -95,10 +87,6 @@ func startClient() error {
 				},
 			})
 		case pause := <-p.NotifyPause():
-			if ignoreSeek.Load() {
-				glog.Debug("pause ignored")
-				continue
-			}
 			glog.Debugf("local: pause state change to %t detected", pause)
 			pos, err := p.GetPlaybackPos()
 			if err != nil {
@@ -125,7 +113,7 @@ func startClient() error {
 	}
 }
 
-func recvChanges(cl protocol.UniView_RoomClient, p player.Interface, ignoreSeek *atomic.Bool) {
+func recvChanges(cl protocol.UniView_RoomClient, p player.Interface) {
 	for {
 		ev, err := cl.Recv()
 		if err != nil {
@@ -136,7 +124,6 @@ func recvChanges(cl protocol.UniView_RoomClient, p player.Interface, ignoreSeek 
 		glog.Debugf("received %s", ev.Type)
 
 		<-p.PlayerReady()
-		ignoreSeek.Store(true)
 		switch ev.Type {
 		case protocol.EventType_EVENT_PAUSE:
 			pauseEv := ev.GetPauseEvent()
@@ -159,7 +146,5 @@ func recvChanges(cl protocol.UniView_RoomClient, p player.Interface, ignoreSeek 
 			glog.Debugf("remote: jump to %s", jumpEv.Timestamp.AsDuration())
 			p.Seek(jumpEv.Timestamp.AsDuration())
 		}
-		time.Sleep(50 * time.Millisecond)
-		ignoreSeek.Store(false)
 	}
 }

internal/player/mpv/actions.go
@@ -16,6 +16,7 @@ func (p *MPV) Pause(state bool) error {
 	}
 	req := rand.Int()
 	glog.Tracef("sending command to set pause to %t", state)
+	p.dropPause.Store(true)
 	p.send(command{
 		Command:   []any{"set_property", "pause", state},
 		RequestID: req,
@@ -52,6 +53,7 @@ func (p *MPV) Seek(ts time.Duration) error {
 	}
 	req := rand.Int()
 	glog.Tracef("sending command to seek to %s", ts)
+	p.dropSeek.Store(true)
 	p.send(command{
 		Command:   []any{"set_property", "time-pos", float64(ts/time.Millisecond) / 1000},
 		RequestID: req,

internal/player/mpv/type.go
@@ -33,7 +33,9 @@ type MPV struct {
 	notifyIdle         chan struct{}
 	commands           chan command
 
-	dead atomic.Bool
+	dead      *atomic.Bool
+	dropSeek  *atomic.Bool
+	dropPause *atomic.Bool
 }
 
 func New() (*MPV, error) {
@@ -72,6 +74,9 @@ func New() (*MPV, error) {
 		quitchan:           make(chan struct{}, 1),
 		playerReady:        make(chan struct{}),
 		commands:           make(chan command, 16),
+		dead:               &atomic.Bool{},
+		dropSeek:           &atomic.Bool{},
+		dropPause:          &atomic.Bool{},
 	}
 	p.cmd = exec.Command(mpvPath, "--input-ipc-server="+socketPath, "--player-operation-mode=pseudo-gui", "--idle")
 	if err := p.cmd.Start(); err != nil {

commit 93001fd0ac295f37c95520c7809e176dc819f4a5

client: fix seeks not being dropped
Fixes: 84cbb77 ("client: drop remotely triggered seeks and pauses")

internal/player/mpv/monitor.go
@@ -32,7 +32,7 @@ func (p *MPV) monitor() {
 
 		switch {
 		case res.Event == "seek":
-			if !p.dropSeek.Load() {
+			if p.dropSeek.Load() {
 				glog.Trace("dropped seek")
 				p.dropSeek.Store(false)
 				continue