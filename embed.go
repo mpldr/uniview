d9c722e5e2dca6feac60b8afca4c005c5e4b1e07
Moritz Poldrack <git@moritz.sh>
2023-09-14 15:05:55 +0200

    chore: add go embed file

--- embed.go
+++ embed.go
@@ -1,8 +1,9 @@
-//go:build tools
-
 package templates
 
 // SPDX-FileCopyrightText: © nobody
 // SPDX-License-Identifier: CC0-1.0
 
-import _ "github.com/tdewolff/minify/v2/cmd/minify"
+import "embed"
+
+//go:embed dist/*
+var templates embed.FS
